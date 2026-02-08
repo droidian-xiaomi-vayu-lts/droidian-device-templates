60bb4b319bc4ec82f29f7206c815780ccbdff4f4
berbascum <berbascum@ticv.cat>
2026-02-08 03:05:24 +0100

    [github] setenv: add extra outputs depends installation
    Signed-off-by: berbascum <berbascum@ticv.cat>

--- setenv-gh-container.sh
+++ setenv-gh-container.sh
@@ -3,3 +3,25 @@
 cp -v device-templates/community_devices.yml .
 
 cp -rv device-templates/apt-config/* pre-overlay/
+
+#set -e
+
+sources_dir="/buildd/sources"
+
+## Update apt
+apt-get update
+
+## Install prereqs
+apt-get install vim git mkbootimg file -y
+
+## Install berb-mkbootmg-mgr
+branch="testing"
+url="https://raw.githubusercontent.com/berbascum/berb-android-mkbootimg-mgr/refs/heads/${branch}"
+bin_script="berb-mkbootimg-mgr.sh"
+cfg_script="mkbootimg-config-vayu.sh"
+## Download binary to bin system path
+wget ${url}/${bin_script} -O /usr/bin/${bin_script%.sh} || error "Download ${bin_script} failed!"
+chmod +x /usr/bin/${bin_script%.sh}
+
+## Download berb-mkboot-mgr vayu conf to current dir
+wget ${url}/${cfg_script} || error "Download ${cfg_script} failed!"
